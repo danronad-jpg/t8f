e933269faecb5f51d1ee92309b4ea8a022086882
danronad-jpg <danronad@gmail.com>
2026-05-01 22:36:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-const colorButtons = document.querySelectorAll('.color-btn');
-const modelViewer = document.querySelector('#t8f-model');
+window.addEventListener('load', () => {
+    const colorButtons = document.querySelectorAll('.color-btn');
+    const modelviewer = document.querySelector('#t8f-model');
 
-colorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // 1. Убираем класс active у всех кнопок
-        colorButtons.forEach(btn => btn.classList.remove('active'));
-        // 2. Добавляем active только той, на которую нажали
-        button.classList.add('active');
+    colorButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // 1. Убираем выделение со всех кнопок и даем активной
+            colorButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
 
-        // 3. Красим раму 
-        const colorString = button.dataset.color;
-        const materials = modelViewer.model.materials;
-        if (materials) {
-            materials[0].pbrMetallicRoughness.setBaseColorFactor(colorString);
-        }
+            // 2. Берем HEX-цвет напрямую из HTML (например, "#ea071a")
+            const colorString = button.dataset.color; 
+
+            // 3. Проверяем, загрузилась ли модель, и красим
+            const model = modelviewer.model;
+            if (model) {
+                const material = model.materials.find(m => m.name === 'Body_Paint');
+                if (material) {
+                    material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+                } else {
+                    console.warn("Материал 'Body_Paint' не найден. Проверь имя в Blender!");
+                }
+            }
+        });
     });
 });
-
 const music = document.getElementById('bg-music');
 const vinyl = document.getElementById('vinyl-record');
 
 // Твой список треков в папке assets
 const playlist = [
     'assets/main_theme_1.mp3',
-    'assets/montagem_estilo_livre.mp3',
+    'assets/main_theme_2.mp3',
     'assets/main_theme_3.mp3',
-    'assets/main_theme_2.mp3'
+    'assets/montagem_estilo_livre'
 ];
 
 let currentTrack = 0;
